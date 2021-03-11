8dcb35b82f932e9065f8d7597d8f61892c7e84ff
Will Burke <billyburke1012@gmail.com>
2021-03-11 14:08:15 -0500

    fib, queue and weave algos

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,60 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//iterative
+// function fib(n) {
+//     const results = [0,1];
+
+//     for (let i = 2; i <= n; i ++){
+//         const a = results[i -1];
+//         const b = results[i-2];
+        
+//         results.push(a+b);
+//     }
+//     return results[n];
+// }
+
+// console.log(fib(4))
+
+// module.exports = fib;
+
+//recursion
+
+//  
+
+//memoization ccan imprive the run time of a recursive statement
+//store the arguemnts of each function along wth the result. 
+// if the function is called again with the same arguments, return the precomputed result, 
+//rather than running the function again
+
+
+function memoize(fn){
+    const cache = {};
+    return function(...args){
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+    return (fib(n-1) + fib(n-2));
+    
+}
+//make sure to call the memoized version of the function
+const fib = memoize(slowFib) 
+
+
 
 module.exports = fib;
+
+
+
