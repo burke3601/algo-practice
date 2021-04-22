4f07989483729db9551d063644d1990e46ce493e
Will Burke <billyburke1012@gmail.com>
2021-04-21 21:45:41 -0400

    binary search tree

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,7 +11,7 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {
+class Node { 
 
     constructor(data) {
         this.data = data;
